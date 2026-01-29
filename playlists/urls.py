8b5c4c79e5fb45c14f8f6df558de9f1cd915600a
jsdev305 <jsdev305@gmail.com>
2026-01-29 12:09:00 -0500

    added playlist detail url , view , and template

--- playlists/urls.py
+++ playlists/urls.py
@@ -5,4 +5,5 @@ app_name = "playlists"
 
 urlpatterns = [
     path("", views.my_playlists, name="my_playlists"),
+    path("<uuid:playlist_id>/", views.playlist_detail, name="playlist_detail"),
 ]
--- playlists/views.py
+++ playlists/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import Playlist
 
+
 @login_required
 def my_playlists(request):
     playlists = Playlist.objects.filter(owner=request.user).order_by("-updated_at")
-    return render(request, "playlists/my_playlists.html", {"playlists" : playlists})
+    return render(request, "playlists/my_playlists.html", {"playlists": playlists})
+
+
+@login_required
+def playlist_detail(request, playlist_id):
+    playlist = get_object_or_404(Playlist, id=playlist_id, owner=request.user)
+
+    # items come from the related_name="items" on PlaylistItem.playlist
+    items = playlist.items.all().order_by("position")
+
+    return render(
+        request,
+        "playlists/playlist_detail.html",
+        {"playlist": playlist, "items": items},
+    )
